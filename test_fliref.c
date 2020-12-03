57f0a209ab6b540f520a3ce723fb48c9522b4861
Zefuri <ciprianu98@gmail.com>
2020-12-03 11:15:47 +0100

    Correction traitement du négatifs dans fliref

--- test_fliref.c
+++ test_fliref.c
@@ -10,12 +10,16 @@
 
 int main(int argc, char *argv[]){
     FICHIER *f2;
+
     char c;
     int  d;
     char my_word[128];
+
     f2 = ouvrir("src_fliref.txt", 'L');
+
     fliref(f2, "\ttest %c end %s", &c, my_word);
     fecriref(stdout, "Read '%c' and '%s' from %s\n", c, my_word, "src_fliref.txt");
+
     fliref(f2, "end %d %s", &d, my_word);
     fecriref(stdout, "Now read '%d' and '%s' from %s\n", d, my_word, "src_fliref.txt");
 
